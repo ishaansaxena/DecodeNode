06ff0ca8b514ec62d6b7453f4a9f8779ea2709a5
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-12 20:31:28 +0530

    Fix foreign key error

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from decode.models import Level 
 
 
 class UserData(models.Model):
     user = models.OneToOneField(User, related_name='details', on_delete=models.CASCADE)
-    current_level = models.ForeignKey('decode.Level', on_delete=models.CASCADE)
+    current_level = models.ForeignKey(Level, default=Level.get_default().pk, on_delete=models.CASCADE, related_name='level')
     current_level_time = models.DateTimeField(default=timezone.now)
     profile_picture = models.ImageField(upload_to='static/assets/user_images/', blank=True)
     address = models.TextField(max_length=500, blank=True)
--- decode/models.py
+++ decode/models.py
@@ -8,7 +8,23 @@ class Level(models.Model):
     text = models.TextField(max_length=250)
     image = models.ImageField(default=None, blank=True)
     comment = models.CharField(max_length=200)
-    answer = models.TextField(max_length=50, blank=True)
+    #answer = models.TextField(max_length=50, blank=True)
 
     def __str__(self):
         return str(self.uid) + u" - " + self.title
+
+    @classmethod
+    def set_default(self):
+        self.default_uid = 1
+        self.default_level = self(uid=self.default_uid)
+        self.default_pk = 1
+        self.default_level.pk = self.default_pk       
+        self.default_level.save()
+
+    @classmethod
+    def get_default(self):
+        try:
+            return self.default_level
+        except AttributeError:
+            self.set_default()
+            return self.default_level
