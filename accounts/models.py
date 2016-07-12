6c494ef5fec567adff58faf5e25005d1361b084d
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-13 04:22:23 +0530

    Add User Data creation on user save

--- accounts/models.py
+++ accounts/models.py
@@ -9,14 +9,10 @@ from decode.models import Level
 
 class UserData(models.Model):
     user = models.OneToOneField(User, related_name='details', on_delete=models.CASCADE)
-    user_level = models.ForeignKey(Level, related_name='level', default=Level.objects.get(pk=1).pk)
-    current_level = models.IntegerField(default=1)
+    current_level = models.ForeignKey(Level, related_name='level', default=Level.objects.get(pk=1).pk)
     current_level_time = models.DateTimeField(default=timezone.now)
     profile_picture = models.ImageField(upload_to='static/assets/user_images/', blank=True)
     is_banned = models.BooleanField(default=False)
-
-    def __init__(self):
-        super(UserData, self).__init__()
 
     def __str__(self):
         return self.user.username
@@ -30,4 +26,5 @@ def create_user_data(sender, instance, created, **kwargs):
     if created:  
        profile, created = UserData.objects.get_or_create(user=instance)  
 
-post_save.connect(create_user_data, sender=User) 
+post_save.connect(create_user_data, User) 
+        
