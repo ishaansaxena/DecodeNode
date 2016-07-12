81cce44d783d92d752af36098d9eeb1333d36bd4
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-13 05:28:25 +0530

    Add namespace to all URLs

--- DecodeNode/urls.py
+++ DecodeNode/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('decode.urls')),
+    url(r'^', include('decode.urls', namespace='decode')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^admin/', admin.site.urls),
 ]
