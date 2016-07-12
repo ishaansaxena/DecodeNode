b46c2cf22b10b95f6806796f2097b03ff9cf73d1
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-13 05:26:51 +0530

    Add play URL

--- decode/urls.py
+++ decode/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.question, name='question_page'),
+    url(r'^play/$', views.play, name='play'),
 ]
