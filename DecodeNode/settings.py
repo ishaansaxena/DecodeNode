6b5a29e2e264b2955b6e48387b17dd66e62c0a27
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-14 03:05:46 +0530

    Change static file config

--- DecodeNode/settings.py
+++ DecodeNode/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # URLs
 LOGIN_URL = 'accounts:login'
