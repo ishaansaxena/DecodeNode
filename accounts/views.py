ee9842fca04d4380d27d51fc3f92481139b3119b
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-14 03:05:20 +0530

    Write preliminary views for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+    current_user = User.objects.get(id=request.user.id)
+    if request.method == 'GET':
+        return render(request, 'accounts/profile.html', {'user': current_user})
+    else:
+        pass
+
+def leaderboard(request):
+    top = User.objects.extra(select={"level":"COALESCE(current_level.uid, current_level_time)"}, order_by=["-current_price"])[:]
+    return render(request, 'accounts/leaderboard.html', {'leaderboard': top})
