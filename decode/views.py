560bbcb4555235bf745f8dda23c256ef70611849
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-13 06:57:46 +0530

    Secure play views

--- decode/views.py
+++ decode/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Level
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import Level
+from .forms import GameForm
+
 
 def index(request):
     context = {}
@@ -8,8 +11,32 @@ def index(request):
 
 @login_required
 def play(request):
-    if request.user.details.is_banned:
+    current_user = User.objects.get(username=request.user.username)
+    if current_user.details.is_banned:
         return render(request, 'decode/banned.html', {})
     else:
-        level = request.user.details.current_level
-        return render(request, 'decode/play.html', {'level': level})
+        level = current_user.details.current_level
+        form = GameForm(request.POST or None)
+        context = {
+            'level': level,
+            'form': form
+        }
+        if request.method == 'GET':
+            return render(request, 'decode/play.html', context)
+        elif request.method == 'POST':
+            if form.is_valid():
+                if form.cleaned_data['user_answer'] == level.answer:
+                    next_level_uid = level.uid + 1
+                    current_user.details.current_level = Level.objects.get(pk=next_level_uid)
+                    level = current_user.details.current_level
+                    current_user.is_banned = True
+                    form.save()
+                    current_user.save(force_update=True)
+                    context['level'] = level
+                else:
+                    context['errors'] = "Wrong Answer."
+                return render(request, 'decode/play.html', context)
+            else:
+                current_user.details.is_banned = True
+                current_user.save()
+                return render(request, 'decode/banned.html')
