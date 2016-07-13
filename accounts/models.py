1e15123737fed70b99832c55247c0026b3f3fddb
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-13 07:30:26 +0530

    Update method set_level

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,15 @@
 from __future__ import unicode_literals
 
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.db import models
-from django.utils import timezone
 from decode.models import Level
 
 
 class UserData(models.Model):
     user = models.OneToOneField(User, related_name='details', on_delete=models.CASCADE)
-    current_level = models.ForeignKey(Level, related_name='level', default=Level.objects.get(pk=1).pk)
+    current_level = models.ForeignKey(Level, default=Level.objects.get(pk=1).pk)
     current_level_time = models.DateTimeField(default=timezone.now)
     profile_picture = models.ImageField(upload_to='static/assets/user_images/', blank=True)
     is_banned = models.BooleanField(default=False)
@@ -17,9 +17,9 @@ class UserData(models.Model):
     def __str__(self):
         return self.user.username
 
-    def set_level(self):
-        level_id = current_level
-        user_level = Level.objects.get(pk=level_id)
+    def set_level(self, level_id):
+        self.current_level = Level.objects.get(pk=level_id)
+        self.current_level_time = timezone.now()
 
 
 def create_user_data(sender, instance, created, **kwargs):  
