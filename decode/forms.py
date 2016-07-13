fa2c36468b7bdb1fc7876b08e2d40d6b12fc6a56
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-14 03:05:59 +0530

    Remove stale class methods

--- decode/forms.py
+++ decode/forms.py
@@ -4,5 +4,3 @@ from django import forms
 class GameForm(forms.Form):
     user_answer = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Answer'}))
 
-    def get_answer(self):
-        return self.user_answer
