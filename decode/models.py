998051c19b4cae7debf1721d70a074e9f5b99cf2
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-15 00:33:00 +0530

    Add default location for ImageField uploads

--- decode/models.py
+++ decode/models.py
@@ -6,9 +6,12 @@ class Level(models.Model):
     uid = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=250)
-    image = models.ImageField(upload_to='static/assets/level_images/', default=None, blank=True)
     comment = models.CharField(max_length=200)
     answer = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(
+        upload_to='static/assets/level_images/', 
+        default=None, blank=True
+    )
 
     def __str__(self):
         return str(self.uid)
