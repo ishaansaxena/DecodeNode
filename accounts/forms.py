63fb9d49e6974269224dfba6b2e1413eecd154ce
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-17 00:06:33 +0530

    Add registration from

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class ProfileForm(forms.Form):
@@ -11,6 +11,25 @@ class ProfileForm(forms.Form):
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            field = self.fields.get(field_name)  
+            if field:
+                widget = field.widget
+                if type(widget) is forms.TextInput:
+                    # something
+                    pass
+                elif type(widget) is forms.Textarea:
+                    # widget.attrs['class'] = 'form-control'
+                    pass
+                widget.attrs['placeholder'] = field.label
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
+    # institute = forms.CharField(label='Institute')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
         for field_name in self.fields:
             field = self.fields.get(field_name)  
             if field:
