e389343b91f9fb4e9f31548442741d73f52ab281
Ishaan Saxena <mail@ishaansaxena.in>
2016-07-18 18:00:32 +0530

    Write functional menu

--- static/js/menu.js
+++ static/js/menu.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
         $('#menu').toggleClass('visible');
         $('#container').toggleClass('hidden');
     });
+    $('#container').click(function(){
+        if ($('#menu').hasClass('visible')){
+            $('#menu').removeClass('visible');
+            $('#container').removeClass('hidden');
+        }
+    });
+    $('.menu-header').each(function(){
+        console.log($(this).attr('data'));
+    });
 });
